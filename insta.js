09d57ee7d44859923cd13d33b993a68d8614491f
sandhyakoyiladinne <158451096+sandhyakoyiladinne@users.noreply.github.com>
2024-05-01 10:52:23 +0530

    Add files via upload

---
+++ insta.js
@@ -0,0 +1,8 @@
+document.getElementById("loginform").addEventListener('submit',function(event){
+event.preventDefault();
+const username=document.getElementById('username').value
+const password=document.getElementById('password').value
+console.log("Username or Email: " + username);
+console.log("Password: " + password);
+})
+
