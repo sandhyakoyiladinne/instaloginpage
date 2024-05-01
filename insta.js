3cda560046c37627ba1db3fb2365dfe4bdbca9fc
sandhyakoyiladinne <158451096+sandhyakoyiladinne@users.noreply.github.com>
2024-05-01 15:03:19 +0530

    Update insta.js

--- insta.js
+++ insta.js
@@ -1,7 +1,14 @@
 document.getElementById("loginform").addEventListener('submit',function(event){
-event.preventDefault();
-const username=document.getElementById('username').value
-const password=document.getElementById('password').value
+  event.preventDefault();
+  const username=document.getElementById('username').value
+  const password=document.getElementById('password').value
+  const email=/^ [^\s@]+@[^\s@]+\.[^\s@]+$/
+    if (email.test(username)){
+        console.error("Enter valid email")
+    }
+    if(password.length < 6){
+        console.error("Password should be greater than 6 characters")
+    }
 console.log("Username or Email: " + username);
 console.log("Password: " + password);
 })
